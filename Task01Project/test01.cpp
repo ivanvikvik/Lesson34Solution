978bd1c3188cc7b8f55f493aa94237772d674212
ivanvikvik <ivanvikvik@gmail.com>
2025-02-16 18:05:22 +0300

    Add project files.

---
+++ Task01Project/test01.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+// global variable
+// int c = 0;
+
+int test(int c) {
+	// local variable
+	// int a = 10;
+	c++;
+	cout << c << endl;
+	return c;
+}
+
+int main() {
+	int c = 0;
+	c = test(c);
+	c = test(c);
+	c = test(c);
+	return 0;
+}
